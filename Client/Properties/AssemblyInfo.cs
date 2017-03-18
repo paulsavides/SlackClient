38fd2ac9b2535d234ea54d70c8ca17428d10b586
theonorth <paulsavides@outlook.com>
2017-03-18 11:57:18 -0500

    Adding .nuspec and updating the assembly information

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SlackBot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Pisces.SlackBot.Client")]
+[assembly: AssemblyDescription("A C# Slack Client.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SlackBot")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
